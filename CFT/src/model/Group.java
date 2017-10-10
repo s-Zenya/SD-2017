b05bddce659b6e85609aacbbd98fa5f1c17a14be
QED495 <b5367@oic.jp>
2017-10-10 16:24:54 +0900

    model、DAO作成

---
+++ CFT/src/model/Group.java
@@ -0,0 +1,39 @@
+package model;
+
+public class Group {
+	private String groupId;
+	private String groupPw;
+	private String groupName;
+
+	public Group( String groupId, String groupPw, String groupName){
+		this.groupId=groupId;
+		this.groupPw=groupPw;
+		this.groupName=groupName;
+	}
+
+	public String getGroupId(){
+		return groupId;
+	}
+
+	public void setGroupId(String groupId){
+		this.groupId = groupId;
+	}
+
+	public String getGroupPw(){
+		return groupPw;
+	}
+
+	public void setGroupPw(String groupPw){
+		this.groupPw = groupPw;
+	}
+
+	public String getGroupName(){
+		return groupName;
+	}
+
+	public void setGroupName(String gropName){
+		this.groupName = groupName;
+	}
+
+
+}
--- CFT/src/model/Personal.java
+++ CFT/src/model/Personal.java
@@ -1,32 +1,49 @@
 package model;
 
 public class Personal {
-	
+
 	private String id;
 	private String pw;
 	private String name;
 	private String groupId;
-	
+
 	public Personal(String id, String pw, String name, String groupId){
 		this.id=id;
 		this.pw=pw;
 		this.name=name;
 		this.groupId=groupId;
 	}
-	
+
 	public String getId(){
 		return id;
 	}
-	
+
+	public void setId(String id){
+		this.id = id;
+	}
+
 	public String getPw(){
 		return pw;
 	}
-	
+
+	public void setPw(String pw){
+		this.pw = pw;
+	}
+
 	public String getName(){
 		return name;
 	}
-	
+
+	public void setName(String name){
+		this.name = name;
+	}
+
 	public String getGroupId(){
 		return groupId;
 	}
+
+	public void setGroupId(String groupId){
+		this.groupId = groupId;
+	}
+
 }
