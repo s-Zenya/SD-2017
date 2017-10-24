e015c32d5069f435ac538c71e073561d2f48090c
QED495 <b5367@oic.jp>
2017-10-24 14:49:19 +0900

    TodoDAO関数

---
+++ CFT/WebContent/assets/javascript/group.js
@@ -0,0 +1,54 @@
+function groupCreate(){
+
+	var gid=document.getElementById("createGroupId").value
+	var gpw=document.getElementById("createGroupPass").value
+	var gname=document.getElementById("createGroupName").value
+    	fetch('/CFT/GroupCreate', {
+    		method: 'POST',
+			body : 'gid='+gid+'&gpw='+gpw+'&gname='+gname,
+			headers : new Headers({'Content-type' : 'application/x-www-form-urlencoded' })
+    	})
+    	  .then(response => {
+    		  console.log(response.status);
+    	    return response.text();
+    	  })
+
+}
+
+function groupLogin(){
+
+	var gid=document.getElementById("LoginGroupId").value
+	var gpw=document.getElementById("LoginGroupPass").value
+	var cookie_all = document.cookie;
+	var id;
+	var tag;
+	var i=0;
+
+	while(cookie_all.split( '; ' )[ i ] != null){
+		if(cookie_all.split( '; ' )[ i ].split( '=' )[ 0 ] == 'id'){
+			id=cookie_all.split('; ')[ i ].split( '=' )[ 1 ];
+			console.log("i="+i);
+		}
+		i++;
+	}
+
+	var date1 = new Date();
+  date1.setTime(0);
+  //有効期限を過去にして書き込む
+  document.cookie = "gid=;expires="+date1.toGMTString();
+
+    	fetch('/CFT/GroupLogin', {
+    		redirect: 'follow',
+    		method: 'POST',
+			body : 'id='+id+'&gid='+gid+'&gpw='+gpw,
+			headers : new Headers({'Content-type' : 'application/x-www-form-urlencoded' }),
+    	})
+    	  .then(response => {
+    		  console.log(response);
+					if(response.status=="200"){
+						 location.href=response.url;
+					}
+    	    return response.text();
+    	  })
+
+}
---
+++ CFT/src/servlet/GroupCreate.java
@@ -0,0 +1,49 @@
+package servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import dao.GroupDAO;
+
+@WebServlet("/GroupCreate")
+public class GroupCreate extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+//		System.out.println(request);
+		String gid = request.getParameter("gid");
+		String gpw = request.getParameter("gpw");
+		String gname = request.getParameter("gname");
+
+		System.out.print(" id="+gid);
+		System.out.print(" pass="+gpw);
+		System.out.print(" name="+gname);
+
+		// ユーザー作成成功
+		GroupDAO groupDAO = new GroupDAO();
+		if (groupDAO.add(gid, gpw, gname)) {
+			response.sendError(HttpServletResponse.SC_OK);
+
+
+		} else {
+			response.sendError(HttpServletResponse.SC_CONFLICT);
+
+		}
+
+	}
+
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.sendError(HttpServletResponse.SC_NOT_FOUND);
+
+	}
+
+}
