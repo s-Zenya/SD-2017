3eb8007ace60938a71505de7f1a2baa6281d5bdd
k-tsutsumi <B5103@oic.jp>
2017-11-24 11:25:51 +0900

    personalLogin personalCreateの入力エラーチェック

--- CFT/WebContent/assets/javascript/personalCreate.js
+++ CFT/WebContent/assets/javascript/personalCreate.js
@@ -9,8 +9,45 @@ function personalCreate(){
 			headers : new Headers({'Content-type' : 'application/x-www-form-urlencoded;charset=UTF-8' })
     	})
     	  .then(response => {
-    		  console.log(response.status);
+//    		  console.log(response.status);
+    		  errorCheck(response.status);
+    		  
     	    return response.text();
     	  })
 
+}
+
+function errorCheck(responseStatus){
+	console.log(responseStatus);
+	
+	document.getElementById("idLabel").style.color = "black";
+	document.getElementById("pwLabel").style.color = "black";
+	document.getElementById("nameLabel").style.color = "black";
+	// 成功
+	if(responseStatus == 200){
+		alert("アカウントを作成しました。");
+	}
+	// 失敗
+	else{
+		var id=document.getElementById("id").value
+		var pw=document.getElementById("pw").value
+		var name=document.getElementById("name").value
+		
+		// 文字数確認
+		if(id.length >= 11 || pw.length >= 11 || name.length >= 31){
+			if(id.length >= 11){document.getElementById("idLabel").style.color = "red"}
+			if(pw.length >= 11){document.getElementById("pwLabel").style.color = "red"}
+			if(name.length >= 31){document.getElementById("nameLabel").style.color = "red"}
+	        alert("error：赤く表示されている欄の文字数を確認してください。");
+	        return;
+		}
+		// ID欄が空白じゃないか
+		else if(id.length == 0){
+			document.getElementById("idLabel").style.color = "red";
+			alert("error：IDを入力してください。");
+			return;
+		}
+		
+		alert("error：アカウントを作成できませんでした。");
+	}
 }
--- CFT/WebContent/assets/javascript/personalLogin.js
+++ CFT/WebContent/assets/javascript/personalLogin.js
@@ -59,7 +59,7 @@ function personalLogin(){
 	while(cookie_all.split( '; ' )[ i ] != null){
 		if(cookie_all.split( '; ' )[ i ].split( '=' )[ 0 ] == 'id'){
 			id=cookie_all.split('; ')[ i ].split( '=' )[ 1 ];
-			console.log("i="+i);
+//			console.log("i="+i);
 		}
 		i++;
 	}
@@ -79,13 +79,46 @@ function personalLogin(){
 			headers : new Headers({'Content-type' : 'application/x-www-form-urlencoded;charset=UTF-8' })
     	})
     	  .then(response => {
-    		  console.log(response);
-//    		  console.log("ff");
-//    		  alert("f");
+//    		  console.log("::::"+response.status);
+    		  errorCheck(response.status);
 					if(response.status=="200"){
 						 location.href=response.url;
 					}
 //    	    return response.text();
     	  })
 
+}
+
+function errorCheck(responseStatus){
+	console.log(responseStatus);
+	
+	document.getElementById("idLabel").style.color = "black";
+	document.getElementById("pwLabel").style.color = "black";
+	
+	// 成功
+	if(responseStatus == 200){
+		alert("ログインしました。\nトップページへ移動します。");
+	}
+	// 失敗
+	else{
+		var id=document.getElementById("id").value
+		var pw=document.getElementById("pw").value
+		
+		// 文字数確認
+		if(id.length >= 11 || pw.length >= 11){
+			if(id.length >= 11){document.getElementById("idLabel").style.color = "red"}
+			if(pw.length >= 11){document.getElementById("pwLabel").style.color = "red"}
+
+	        alert("error：赤く表示されている欄の文字数を確認してください。");
+	        return;
+		}
+		// ID欄が空白じゃないか
+		else if(id.length == 0){
+			document.getElementById("idLabel").style.color = "red";
+			alert("error：IDを入力してください。");
+			return;
+		}
+		
+		alert("error：ログインできませんでした。\nIDまたはPWを確認してください。");
+	}
 }
