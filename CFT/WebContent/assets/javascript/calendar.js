0711306add5aeb3ae339520167a98001114fee28
QED495 <b5367@oic.jp>
2017-12-09 00:41:46 +0900

    edit top

--- CFT/WebContent/assets/javascript/calendar.js
+++ CFT/WebContent/assets/javascript/calendar.js
@@ -1,98 +1,98 @@
- // showCalendar();
- // addCalendar();
- var NowDate=new Date();
- //(例)'2017-11-01'
- var dateStr=NowDate.getFullYear()+'-'+("0"+(NowDate.getMonth()+1)).slice(-2)+'-'+("0"+NowDate.getDate()).slice(-2);
+// showCalendar();
+// addCalendar();
+var NowDate=new Date();
+//(例)'2017-11-01'
+var dateStr=NowDate.getFullYear()+'-'+("0"+(NowDate.getMonth()+1)).slice(-2)+'-'+("0"+NowDate.getDate()).slice(-2);
 // getCalendar(dateStr);
 
 function showCalendar(planObj,dateMain){
-	var displayYear = [];//先月、今月、来月の西暦が入る
-	var displayMon = [];//先月、今月、来月の月が入る
-	var startMon = dateMain.substr(5,2)-1;
-	for(var i=0;i<3;i++){
-		displayYear[i]=dateMain.substr(0,4);
-		displayMon[i]=startMon+i%13;
-		if(displayMon[i]==0){//月の繰り下がりの処理
-			displayYear[i]--;
-			displayMon[i]=12;
-		}else if(displayMon[i]==13){//月の繰り上がりの処理
-			displayYear[i]++;
-			displayMon[i]=1;
-		}
-	}
-	//データの削除
-	for(var i=0;i<3;i++){
-		for(var j=1;j<=31;j++){
-			$('#'+displayYear[i]+'-'+("0"+displayMon[i]).slice(-2)+'-'+("0"+j).slice(-2)).empty();
-		}
-	}
-	//htmlへ書き込み
-	for(let k in planObj) {
-		var addCalendarHtml="";
-		addCalendarHtml+='<button id="plans" class="btn btn-mini btn-plan" data-toggle="modal" data-target="#modal_'+planObj[k].calendarId+'">'+planObj[k].content+'</button><br>';
-		addCalendarHtml+='<div class="modal" id="modal_'+planObj[k].calendarId+'" tabindex="-1">';
-		addCalendarHtml+='<div class="modal-dialog">';
-		addCalendarHtml+='<div class="modal-content">';
-		addCalendarHtml+='<div class="modal-header">';
-		addCalendarHtml+='<h4 class="modal-title" id="modal-label">詳細</h4>';//ここヘッダー
-		addCalendarHtml+='<button type="button" class="close" data-dismiss="modal">';
-		addCalendarHtml+='<span aria-hidden="true">&times;</span>';//閉じるボタン(右上)
-		addCalendarHtml+='</button>';
-		addCalendarHtml+='</div>';
-		addCalendarHtml+='<div class="modal-body">';//ここからモーダルのbody
-		addCalendarHtml+='<span id="calendar_plan_form_'+planObj[k].calendarId+'">日付:'+planObj[k].date+'</span><br>';
-		addCalendarHtml+='<span>作成者:'+planObj[k].name+'</span><br>';
-		addCalendarHtml+='<span>内容:'+planObj[k].content+'</span><br>';
-		addCalendarHtml+='</div>';
-		addCalendarHtml+='<div class="modal-footer">';
-		addCalendarHtml+='<button type="button" class="btn btn-default" data-dismiss="modal">閉じる</button>';
-		addCalendarHtml+='<button type="button" class="btn btn-primary" onclick = "deleteCalendar('+planObj[k].calendarId+')"  data-dismiss="modal">削除</button>';
-		addCalendarHtml+='</div>';
-		addCalendarHtml+='</div>';
-		addCalendarHtml+='</div>';
-		addCalendarHtml+='</div>';
-		$('#'+planObj[k].date).append(addCalendarHtml);
-	}
+  var displayYear = [];//先月、今月、来月の西暦が入る
+  var displayMon = [];//先月、今月、来月の月が入る
+  var startMon = dateMain.substr(5,2)-1;
+  for(var i=0;i<3;i++){
+    displayYear[i]=dateMain.substr(0,4);
+    displayMon[i]=startMon+i%13;
+    if(displayMon[i]==0){//月の繰り下がりの処理
+      displayYear[i]--;
+      displayMon[i]=12;
+    }else if(displayMon[i]==13){//月の繰り上がりの処理
+      displayYear[i]++;
+      displayMon[i]=1;
+    }
+  }
+  //データの削除
+  for(var i=0;i<3;i++){
+    for(var j=1;j<=31;j++){
+      $('#'+displayYear[i]+'-'+("0"+displayMon[i]).slice(-2)+'-'+("0"+j).slice(-2)).empty();
+    }
+  }
+  //htmlへ書き込み
+  for(let k in planObj) {
+    var addCalendarHtml="";
+    addCalendarHtml+='<button id="plans" class="btn btn-mini btn-plan" data-toggle="modal" data-target="#modal_'+planObj[k].calendarId+'">'+planObj[k].content+'</button><br>';
+    addCalendarHtml+='<div class="modal" id="modal_'+planObj[k].calendarId+'" tabindex="-1">';
+    addCalendarHtml+='<div class="modal-dialog">';
+    addCalendarHtml+='<div class="modal-content">';
+    addCalendarHtml+='<div class="modal-header">';
+    addCalendarHtml+='<h4 class="modal-title" id="modal-label">詳細</h4>';//ここヘッダー
+    addCalendarHtml+='<button type="button" class="close" data-dismiss="modal">';
+    addCalendarHtml+='<span aria-hidden="true">&times;</span>';//閉じるボタン(右上)
+    addCalendarHtml+='</button>';
+    addCalendarHtml+='</div>';
+    addCalendarHtml+='<div class="modal-body">';//ここからモーダルのbody
+    addCalendarHtml+='<span id="calendar_plan_form_'+planObj[k].calendarId+'">日付:'+planObj[k].date+'</span><br>';
+    addCalendarHtml+='<span>作成者:'+planObj[k].name+'</span><br>';
+    addCalendarHtml+='<span>内容:'+planObj[k].content+'</span><br>';
+    addCalendarHtml+='</div>';
+    addCalendarHtml+='<div class="modal-footer">';
+    addCalendarHtml+='<button type="button" class="btn btn-default" data-dismiss="modal">閉じる</button>';
+    addCalendarHtml+='<button type="button" class="btn btn-primary" onclick = "deleteCalendar('+planObj[k].calendarId+')"  data-dismiss="modal">削除</button>';
+    addCalendarHtml+='</div>';
+    addCalendarHtml+='</div>';
+    addCalendarHtml+='</div>';
+    addCalendarHtml+='</div>';
+    $('#'+planObj[k].date).append(addCalendarHtml);
+  }
 }
 
 // Topページでのカレンダー描画-
 function showCalendarTop(planObj,dateMain){
-	// var displayYear = [];//先月、今月、来月の西暦が入る
-	// var displayMon = [];//先月、今月、来月の月が入る
-	// var startMon = dateMain.substr(5,2)-1;
-	// for(var i=0;i<3;i++){
-	// 	displayYear[i]=dateMain.substr(0,4);
-	// 	displayMon[i]=startMon+i%13;
-	// 	if(displayMon[i]==0){//月の繰り下がりの処理
-	// 		displayYear[i]--;
-	// 		displayMon[i]=12;
-	// 	}else if(displayMon[i]==13){//月の繰り上がりの処理
-	// 		displayYear[i]++;
-	// 		displayMon[i]=1;
-	// 	}
-	// }
-	// //データの削除
-	// for(var i=0;i<3;i++){
-	// 	for(var j=1;j<=31;j++){
-	// 		$('#'+displayYear[i]+'-'+("0"+displayMon[i]).slice(-2)+'-'+("0"+j).slice(-2)).empty();
-	// 	}
-	// }
+  // var displayYear = [];//先月、今月、来月の西暦が入る
+  // var displayMon = [];//先月、今月、来月の月が入る
+  // var startMon = dateMain.substr(5,2)-1;
+  // for(var i=0;i<3;i++){
+  // 	displayYear[i]=dateMain.substr(0,4);
+  // 	displayMon[i]=startMon+i%13;
+  // 	if(displayMon[i]==0){//月の繰り下がりの処理
+  // 		displayYear[i]--;
+  // 		displayMon[i]=12;
+  // 	}else if(displayMon[i]==13){//月の繰り上がりの処理
+  // 		displayYear[i]++;
+  // 		displayMon[i]=1;
+  // 	}
+  // }
+  // //データの削除
+  // for(var i=0;i<3;i++){
+  // 	for(var j=1;j<=31;j++){
+  // 		$('#'+displayYear[i]+'-'+("0"+displayMon[i]).slice(-2)+'-'+("0"+j).slice(-2)).empty();
+  // 	}
+  // }
   var countPlan = 0;
-	//htmlへ書き込み
-	for(let k in planObj) {
-		var addCalendarHtml="";
-		addCalendarHtml+='<tr>';
-		addCalendarHtml+='<td>'+planObj[k].date+'</td>';
-		addCalendarHtml+='<td>'+planObj[k].content+'</td>';
-		addCalendarHtml+='</tr>';
+  //htmlへ書き込み
+  for(let k in planObj) {
+    var addCalendarHtml="";
+    addCalendarHtml+='<tr>';
+    addCalendarHtml+='<td>'+planObj[k].date+'</td>';
+    addCalendarHtml+='<td>'+planObj[k].content+'</td>';
+    addCalendarHtml+='</tr>';
     if(dateStr<=planObj[k].date){
-  		$('#calendar_table tbody').append(addCalendarHtml);
+      $('#calendar_table tbody').append(addCalendarHtml);
       countPlan++;
     }
     if(20<countPlan){
       break;
     }
-	}
+  }
 }
 
 
@@ -101,154 +101,154 @@ function showCalendarTop(planObj,dateMain){
 
 //予定の追加
 function addCalendar(year,mon,day){
-	var cookie_all = document.cookie;
-	var gid;
-	var date = year+"-"+("0"+mon).slice(-2)+"-"+("0"+day).slice(-2);//ここで日付を取得
-	var content=$("#calendar_content_form_"+date).val();
-	var name;
-	var i=0;
-	if(content.length>0){
-		while(cookie_all.split( '; ' )[ i ] != null){
+  var cookie_all = document.cookie;
+  var gid;
+  var date = year+"-"+("0"+mon).slice(-2)+"-"+("0"+day).slice(-2);//ここで日付を取得
+  var content=$("#calendar_content_form_"+date).val();
+  var name;
+  var i=0;
+  if(content.length>0){
+    while(cookie_all.split( '; ' )[ i ] != null){
 
-			if(cookie_all.split( '; ' )[ i ].split( '=' )[ 0 ] == 'gId'){
-				gid=cookie_all.split('; ')[ i ].split( '=' )[ 1 ];
-			}
-			if(cookie_all.split( '; ' )[ i ].split( '=' )[ 0 ] == 'name'){
-				name=cookie_all.split('; ')[ i ].split( '=' )[ 1 ];
-			}
-			i++;
-		}
-		gid=decodeURIComponent(gid);
-		name=decodeURIComponent(name);
+      if(cookie_all.split( '; ' )[ i ].split( '=' )[ 0 ] == 'gId'){
+        gid=cookie_all.split('; ')[ i ].split( '=' )[ 1 ];
+      }
+      if(cookie_all.split( '; ' )[ i ].split( '=' )[ 0 ] == 'name'){
+        name=cookie_all.split('; ')[ i ].split( '=' )[ 1 ];
+      }
+      i++;
+    }
+    gid=decodeURIComponent(gid);
+    name=decodeURIComponent(name);
 
-   	fetch('/Calendar', {
-   		mode: 'cors', //クロスオリジンリクエストをするのでCORSモードにする
-   		credentials: 'include',
-   		redirect: 'follow',
-   		method: 'POST',
-			body : 'gid='+gid+'&date='+date+'&content='+content+'&name='+name,
-			headers : new Headers({'Content-type' : 'application/x-www-form-urlencoded;charset=UTF-8' })
-   	}).then(response => {
-   		errorCheck(response.status,year,mon,day);
-			if(response.status=="200"){
-				getCalendar(date);
-			}
-			console.dir(response.status);
-			return response.text();
-		});
-	}
+    fetch('/Calendar', {
+      mode: 'cors', //クロスオリジンリクエストをするのでCORSモードにする
+      credentials: 'include',
+      redirect: 'follow',
+      method: 'POST',
+      body : 'gid='+gid+'&date='+date+'&content='+content+'&name='+name,
+      headers : new Headers({'Content-type' : 'application/x-www-form-urlencoded;charset=UTF-8' })
+    }).then(response => {
+      errorCheck(response.status,year,mon,day);
+      if(response.status=="200"){
+        getCalendar(date);
+      }
+      console.dir(response.status);
+      return response.text();
+    });
+  }
 }
 
 //予定の削除
 function deleteCalendar(calendarId){
-	var cookie_all = document.cookie;
-	var calendarId;
-	var i=0;
-	var url= '/Calendar?calendarId='+calendarId;
-   	fetch(url, {
-   		mode: 'cors', //クロスオリジンリクエストをするのでCORSモードにする
-   		credentials: 'include',
-   		redirect: 'follow',
-   		method: 'GET',
-		headers : new Headers({'Content-type' : 'application/x-www-form-urlencoded;charset=UTF-8' })
-   	}).then(response => {
-			if(response.status=="200"){
-				console.log("deleteCalendar OK");
-				getCalendar(dateStr);
-			}
-			console.dir(response.status);
-			return response.text();
-		});
+  var cookie_all = document.cookie;
+  var calendarId;
+  var i=0;
+  var url= '/Calendar?calendarId='+calendarId;
+  fetch(url, {
+    mode: 'cors', //クロスオリジンリクエストをするのでCORSモードにする
+    credentials: 'include',
+    redirect: 'follow',
+    method: 'GET',
+    headers : new Headers({'Content-type' : 'application/x-www-form-urlencoded;charset=UTF-8' })
+  }).then(response => {
+    if(response.status=="200"){
+      console.log("deleteCalendar OK");
+      getCalendar(dateStr);
+    }
+    console.dir(response.status);
+    return response.text();
+  });
 }
 
 //予定の取得
 function getCalendar(date){
-	var cookie_all = document.cookie;
-	var gid;
-	var i=0;
-	if(date!=''){
-		while(cookie_all.split( '; ' )[ i ] != null){
-			if(cookie_all.split( '; ' )[ i ].split( '=' )[ 0 ] == 'gId'){
-				gid=cookie_all.split('; ')[ i ].split( '=' )[ 1 ];
-			}
-			i++;
-		}
-		gid=decodeURIComponent(gid);
-		var url= '/Calendar?gid='+gid+'&date='+date;
-		fetch(url, {
-			mode: 'cors', //クロスオリジンリクエストをするのでCORSモードにする
-			credentials: 'include',
-			redirect: 'follow',
-			method: 'GET',
-			headers : new Headers({'Content-type' : 'application/x-www-form-urlencoded;charset=UTF-8' })
-		}).then(response => {
-			if(response.status=="200"){
-			}
-			return response.text();
-		}).then(text => {
-//    		  予定の書き換え
-			// console.log(text);
-			showCalendar($.parseJSON(text),date);
-		});
-	}
+  var cookie_all = document.cookie;
+  var gid;
+  var i=0;
+  if(date!=''){
+    while(cookie_all.split( '; ' )[ i ] != null){
+      if(cookie_all.split( '; ' )[ i ].split( '=' )[ 0 ] == 'gId'){
+        gid=cookie_all.split('; ')[ i ].split( '=' )[ 1 ];
+      }
+      i++;
+    }
+    gid=decodeURIComponent(gid);
+    var url= '/Calendar?gid='+gid+'&date='+date;
+    fetch(url, {
+      mode: 'cors', //クロスオリジンリクエストをするのでCORSモードにする
+      credentials: 'include',
+      redirect: 'follow',
+      method: 'GET',
+      headers : new Headers({'Content-type' : 'application/x-www-form-urlencoded;charset=UTF-8' })
+    }).then(response => {
+      if(response.status=="200"){
+      }
+      return response.text();
+    }).then(text => {
+      //    		  予定の書き換え
+      // console.log(text);
+      showCalendar($.parseJSON(text),date);
+    });
+  }
 }
 
 
 
 //Top画面用予定の取得
 function getCalendarTop(date){
-	var cookie_all = document.cookie;
-	var gid;
-	var i=0;
-	if(date!=''){
-		while(cookie_all.split( '; ' )[ i ] != null){
-			if(cookie_all.split( '; ' )[ i ].split( '=' )[ 0 ] == 'gId'){
-				gid=cookie_all.split('; ')[ i ].split( '=' )[ 1 ];
-			}
-			i++;
-		}
-		gid=decodeURIComponent(gid);
-		var url= '/Calendar?gid='+gid+'&date='+date;
-		fetch(url, {
-			mode: 'cors', //クロスオリジンリクエストをするのでCORSモードにする
-			credentials: 'include',
-			redirect: 'follow',
-			method: 'GET',
-			headers : new Headers({'Content-type' : 'application/x-www-form-urlencoded;charset=UTF-8' })
-		}).then(response => {
-			if(response.status=="200"){
-			}
-			return response.text();
-		}).then(text => {
-//    		  予定の書き換え
-			// console.log(text);
-			showCalendarTop($.parseJSON(text),date);
-		});
-	}
+  var cookie_all = document.cookie;
+  var gid;
+  var i=0;
+  if(date!=''){
+    while(cookie_all.split( '; ' )[ i ] != null){
+      if(cookie_all.split( '; ' )[ i ].split( '=' )[ 0 ] == 'gId'){
+        gid=cookie_all.split('; ')[ i ].split( '=' )[ 1 ];
+      }
+      i++;
+    }
+    gid=decodeURIComponent(gid);
+    var url= '/Calendar?gid='+gid+'&date='+date;
+    fetch(url, {
+      mode: 'cors', //クロスオリジンリクエストをするのでCORSモードにする
+      credentials: 'include',
+      redirect: 'follow',
+      method: 'GET',
+      headers : new Headers({'Content-type' : 'application/x-www-form-urlencoded;charset=UTF-8' })
+    }).then(response => {
+      if(response.status=="200"){
+      }
+      return response.text();
+    }).then(text => {
+      //    		  予定の書き換え
+      // console.log(text);
+      showCalendarTop($.parseJSON(text),date);
+    });
+  }
 }
 
 
 //入力値チェック
 function errorCheck(responseStatus,year,mon,day){
-	console.log(responseStatus);
-	$("#addComment").remove();
+  console.log(responseStatus);
+  $("#addComment").remove();
 
-	// 成功
-	if(responseStatus == 200){
-		$('h1').append('<div id="addComment"><font color="green"><h4>予定を追加しました。</h4></font></div>');
-	}
-	// 失敗
-	else{
-		var date = year+"-"+("0"+mon).slice(-2)+"-"+("0"+day).slice(-2);//ここで日付を取得
-		var content=$("#calendar_content_form_"+date).val();
+  // 成功
+  if(responseStatus == 200){
+    $('h1').append('<div id="addComment"><font color="green"><h4>予定を追加しました。</h4></font></div>');
+  }
+  // 失敗
+  else{
+    var date = year+"-"+("0"+mon).slice(-2)+"-"+("0"+day).slice(-2);//ここで日付を取得
+    var content=$("#calendar_content_form_"+date).val();
 
-		// 文字数確認
-		if(content.length >= 101 || content.length == 0){
-			$('h1').append('<div id="addComment"><font color="red"><h4>error：入力した文字数を確認してください。</h4></font></div>');
-	        return;
-		}
+    // 文字数確認
+    if(content.length >= 101 || content.length == 0){
+      $('h1').append('<div id="addComment"><font color="red"><h4>error：入力した文字数を確認してください。</h4></font></div>');
+      return;
+    }
 
-		$('h1').append('<div id="addComment"><font color="red"><h4>error：予定を追加できませんでした。</h4></font></div>');
-		return;
-	}
+    $('h1').append('<div id="addComment"><font color="red"><h4>error：予定を追加できませんでした。</h4></font></div>');
+    return;
+  }
 }
--- CFT/WebContent/assets/javascript/header.js
+++ CFT/WebContent/assets/javascript/header.js
@@ -1,11 +1,9 @@
-var headerHTML = '<TABLE border="0" bgcolor="#ffffff" width="100%" cellspacing="0" cellpadding="0" height="50">';
+var headerHTML = '<DIV id="header"><TABLE border="0" bgcolor="#ffffff" width="100%" cellspacing="0" cellpadding="0" height="50">';
 headerHTML += '<TR><TD>&nbsp;<FONT color="#000000" size="5">CFT</FONT>';
-headerHTML += '<FONT color="#424242" size="4">.CommunicationFamliyTool<button id = "head-button" class="btn btn-primary col-sm-2" onClick="recStart()">rec</button><br></FONT></TD></TR>';
-headerHTML += '<TR bgcolor="#424242" height="2"><TD></TD></TR></TABLE>';
+headerHTML += '<FONT color="#424242" size="4">.CommunicationFamliyTool<br></FONT></TD>';
+headerHTML += '<TD id="rec-button" border="0" bgcolor="#ffffff" cellspacing="0" cellpadding="0" height="50"><button id = "head-button" class="btn btn-primary col-sm-12" onClick="recStart()">rec</button></TD>';
+headerHTML += '<TR bgcolor="#424242" height="2"><TD></TD><TD></TD></TR></TABLE></DIV>';
 document.write(headerHTML);
-console.log("hoge");
-
-
 // ==音声認識========================================================================
 var grammar = '#JSGF V1.0; grammar colors; public <color> = aqua | azure | beige | bisque | black | blue | brown | chocolate | coral | crimson | cyan | fuchsia | ghostwhite | gold | goldenrod | gray | green | indigo | ivory | khaki | lavender | lime | linen | magenta | maroon | moccasin | navy | olive | orange | orchid | peru | pink | plum | purple | red | salmon | sienna | silver | snow | tan | teal | thistle | tomato | turquoise | violet | white | yellow ;'
 var recognition = new webkitSpeechRecognition();
@@ -19,164 +17,164 @@ recognition.maxAlternatives = 1; //配列の数
 
 recognition.onresult = function(event) {
   var result = event.results[0][0].transcript;
-	//探索する値
-	var when = [["今日"],["明日","翌日"],["明後日"],[/\d{1,2}日/],[/\d{1,2}月/]];
-	var jWork = [["やること"],["買"],["予定"]];
-	var eWork = ["ToDo","Shopping","Calendar"];
+  //探索する値
+  var when = [["今日"],["明日","翌日"],["明後日"],[/\d{1,2}日/],[/\d{1,2}月/]];
+  var jWork = [["やること"],["買"],["予定"]];
+  var eWork = ["ToDo","Shopping","Calendar"];
 
-	var nowDate=new Date();
-	var year = nowDate.getFullYear();
-	var mon = 0;
-	var date = 0;
-	var tool;
-	var str = "";
-	var speechContent = ''
-	//時間を探索
-	for (var i = 0; i < when.length; i++) {
-		for (var j = 0; j < when[i].length; j++) {
-			if ((result.match(when[i][j]) != null) && (i < 3)) {
-				speechContent += result.match(when[i][j]);
-				nowDate.setDate(nowDate.getDate()+i);
-			}
-			if ((result.match(/\d{1,2}月/) != null) && (0 == mon)) {
-					str = result.match(/\d{1,2}月/);
-					speechContent += str;
-					nowDate.setMonth((str.toString().match(/\d{1,2}/)) -  1);
-          mon = 1;
-			}
-			if ((result.match(/\d{1,2}日/) != null) && (0 == date)) {
-				str = result.match(/\d{1,2}日/).toString();
-				speechContent += str;
-				nowDate.setDate(str.toString().match(/\d{1,2}/));
+  var nowDate=new Date();
+  var year = nowDate.getFullYear();
+  var mon = 0;
+  var date = 0;
+  var tool;
+  var str = "";
+  var speechContent = ''
+  //時間を探索
+  for (var i = 0; i < when.length; i++) {
+    for (var j = 0; j < when[i].length; j++) {
+      if ((result.match(when[i][j]) != null) && (i < 3)) {
+        speechContent += result.match(when[i][j]);
+        nowDate.setDate(nowDate.getDate()+i);
+      }
+      if ((result.match(/\d{1,2}月/) != null) && (0 == mon)) {
+        str = result.match(/\d{1,2}月/);
+        speechContent += str;
+        nowDate.setMonth((str.toString().match(/\d{1,2}/)) -  1);
+        mon = 1;
+      }
+      if ((result.match(/\d{1,2}日/) != null) && (0 == date)) {
+        str = result.match(/\d{1,2}日/).toString();
+        speechContent += str;
+        nowDate.setDate(str.toString().match(/\d{1,2}/));
         date = 1;
-			}
-		}
-	}
-	speechContent += "の";
-	//機能を探索
-	for (var i = 0; i < jWork.length; i++) {
-		for (var j = 0; j < jWork[i].length; j++) {
-			if ( result.indexOf(jWork[i][j]) != -1) {
-				tool = eWork[i];
-			}
-		}
-	}
-	date = nowDate.getDate();
-	year = nowDate.getFullYear();
-	mon = nowDate.getMonth()+1;
-	date = (year+"-"+(("0"+mon).slice(-2))+"-"+(("0"+date).slice(-2))).toString();
-// -----ここに音声取得後の処理-----------------------------------------------------------------------------
+      }
+    }
+  }
+  speechContent += "の";
+  //機能を探索
+  for (var i = 0; i < jWork.length; i++) {
+    for (var j = 0; j < jWork[i].length; j++) {
+      if ( result.indexOf(jWork[i][j]) != -1) {
+        tool = eWork[i];
+      }
+    }
+  }
+  date = nowDate.getDate();
+  year = nowDate.getFullYear();
+  mon = nowDate.getMonth()+1;
+  date = (year+"-"+(("0"+mon).slice(-2))+"-"+(("0"+date).slice(-2))).toString();
+  // -----ここに音声取得後の処理-----------------------------------------------------------------------------
   var cookie_all = document.cookie;
   var gid;
   var i=0;
   if(date!=''){
-  	while(cookie_all.split( '; ' )[ i ] != null){
-  		if(cookie_all.split( '; ' )[ i ].split( '=' )[ 0 ] == 'gId'){
-  			gid=cookie_all.split('; ')[ i ].split( '=' )[ 1 ];
-  		}
-  		i++;
-  	}
-  	gid=decodeURIComponent(gid);
-  	var url= '/'+tool+'?gid='+gid+'&date='+date;
-  	fetch(url, {
-  		mode: 'cors', //クロスオリジンリクエストをするのでCORSモードにする
-  		credentials: 'include',
-  		redirect: 'follow',
-  		method: 'GET',
-  		headers : new Headers({'Content-type' : 'application/x-www-form-urlencoded;charset=UTF-8' })
-  	}).then(response => {
-  		if(response.status=="200"){
-  		}
-  		return response.text();
-  	}).then(text => {
-  //    		  予定の書き換え
-  console.log(text);
-  		var dataObj = $.parseJSON(text);
-  		// ===================================================================================
-  		var i = 0;
-  		if ("ToDo" == tool) {
-  			//speechContentにやることを追加
-  			speechContent += "やることは";
-  			for(let k in dataObj) {
-  				if (dataObj[k].done == "false") {
-  					speechContent += dataObj[k].contents+",";
-  					i = 1;
-  				}
-  			}
-  			if (0 == i) {
-  				speechContent += "ありません";
-  			}
-  			else {
-  				speechContent = speechContent.slice(0,-1);
-  				speechContent += "です。";
-  			}
-  		}
-  		else if ("Shopping" == tool) {
-  			//speechContentに買うものを追加
-  			var replace = 1;
-  			speechContent += "買うものは";
-  			for(let k in dataObj) {
-  				if (dataObj[k].done == "false") {
-  					speechContent += dataObj[k].contents+",";
-  					i = 1;
-  				}
-  			}
-  			for (;;) {
-  				speechContent = speechContent.replace("&nbsp;","");
-  				if (speechContent.indexOf("&nbsp;") == -1) {
-  					break;
-  				}
-  			}
-  			if (0 == i) {
-  				speechContent += "ありません";
-  			}
-  			else {
-  				speechContent = speechContent.slice(0,-1);
-  				speechContent += "です。";
-  			}
+    while(cookie_all.split( '; ' )[ i ] != null){
+      if(cookie_all.split( '; ' )[ i ].split( '=' )[ 0 ] == 'gId'){
+        gid=cookie_all.split('; ')[ i ].split( '=' )[ 1 ];
+      }
+      i++;
+    }
+    gid=decodeURIComponent(gid);
+    var url= '/'+tool+'?gid='+gid+'&date='+date;
+    fetch(url, {
+      mode: 'cors', //クロスオリジンリクエストをするのでCORSモードにする
+      credentials: 'include',
+      redirect: 'follow',
+      method: 'GET',
+      headers : new Headers({'Content-type' : 'application/x-www-form-urlencoded;charset=UTF-8' })
+    }).then(response => {
+      if(response.status=="200"){
+      }
+      return response.text();
+    }).then(text => {
+      //    		  予定の書き換え
+      console.log(text);
+      var dataObj = $.parseJSON(text);
+      // ===================================================================================
+      var i = 0;
+      if ("ToDo" == tool) {
+        //speechContentにやることを追加
+        speechContent += "やることは";
+        for(let k in dataObj) {
+          if (dataObj[k].done == "false") {
+            speechContent += dataObj[k].contents+",";
+            i = 1;
+          }
+        }
+        if (0 == i) {
+          speechContent += "ありません";
+        }
+        else {
+          speechContent = speechContent.slice(0,-1);
+          speechContent += "です。";
+        }
+      }
+      else if ("Shopping" == tool) {
+        //speechContentに買うものを追加
+        var replace = 1;
+        speechContent += "買うものは";
+        for(let k in dataObj) {
+          if (dataObj[k].done == "false") {
+            speechContent += dataObj[k].contents+",";
+            i = 1;
+          }
+        }
+        for (;;) {
+          speechContent = speechContent.replace("&nbsp;","");
+          if (speechContent.indexOf("&nbsp;") == -1) {
+            break;
+          }
+        }
+        if (0 == i) {
+          speechContent += "ありません";
+        }
+        else {
+          speechContent = speechContent.slice(0,-1);
+          speechContent += "です。";
+        }
 
-  		}
-  		else if ('Calendar' == tool) {
-  			//speechContentに予定を追加
-  			speechContent += "予定は";
-  			for(let k in dataObj) {
-  				if (dataObj[k].date == date) {
-  					speechContent += dataObj[k].content+"と";
-  					i = 1;
-  				}
-  			}
-  			if (0 == i) {
-  				speechContent += "ありません";
-  			}
-  			else {
-  				speechContent = speechContent.slice(0,-1);
-  				speechContent += "です。";
-  			}
-  		}
-  		else {
-  			speechContent = "音声が取得できませんでした";
-  		}
-  		say(speechContent);
-  		// showCalendar($.parseJSON(text),date);
-  	});
+      }
+      else if ('Calendar' == tool) {
+        //speechContentに予定を追加
+        speechContent += "予定は";
+        for(let k in dataObj) {
+          if (dataObj[k].date == date) {
+            speechContent += dataObj[k].content+"と";
+            i = 1;
+          }
+        }
+        if (0 == i) {
+          speechContent += "ありません";
+        }
+        else {
+          speechContent = speechContent.slice(0,-1);
+          speechContent += "です。";
+        }
+      }
+      else {
+        speechContent = "音声が取得できませんでした";
+      }
+      say(speechContent);
+      // showCalendar($.parseJSON(text),date);
+    });
   }
 
   function say(content) {
-  	var synthes = new SpeechSynthesisUtterance();
-  	synthes.voiceURI = 'native';
-  	synthes.volume = 1;
-  	synthes.rate = 0.6;
-  	synthes.pitch = 4;
-  	synthes.text = content;
-  	synthes.lang = 'ja-JP';
-  	synthes.onend = function(e) {
-  	    alert(content);
-  	};
-  	speechSynthesis.speak(synthes);
+    var synthes = new SpeechSynthesisUtterance();
+    synthes.voiceURI = 'native';
+    synthes.volume = 1;
+    synthes.rate = 0.6;
+    synthes.pitch = 4;
+    synthes.text = content;
+    synthes.lang = 'ja-JP';
+    synthes.onend = function(e) {
+      alert(content);
+    };
+    speechSynthesis.speak(synthes);
   }
 }
 // ------------------------------------------------------------------------------------------------------
 
 function recStart(){
-	recognition.start();
+  recognition.start();
 }
